fce6c20e329c77c18ceec427a616efc931271d05
Ambrose J. Carr <mail@ambrosejcarr.com>
2019-01-13 10:28:33 -0500

    pause to fix spot finding

--- notebooks/py/BaristaSeq.py
+++ notebooks/py/BaristaSeq.py
@@ -15,7 +15,9 @@ from tqdm import tqdm
 import starfish.data
 import starfish.plot
 from starfish.spots import SpotFinder
-from starfish.types import Indices
+from starfish.types import Axes
+
+# TODO the registration in this dataset isn't working, look at this again.
 
 # ======================================= LOAD THE DATA ===========================================
 
@@ -26,8 +28,8 @@ nissl = fov['nuclei']  # this is actually dots, should change this.
 
 # ===================================== MAX PROJECT OVER Z ========================================
 
-z_projected_image = img.max_proj(Indices.Z)
-z_projected_nissl = nissl.max_proj(Indices.Z)
+z_projected_image = img.max_proj(Axes.ZPLANE)
+z_projected_nissl = nissl.max_proj(Axes.ZPLANE)
 
 # ====================================== ALIGN C CHANNEL ==========================================
 
@@ -41,11 +43,11 @@ rounds = np.arange(img.num_rounds)
 slice_indices = product(channels, rounds)
 
 for ch, round_, in slice_indices:
-    indices = {Indices.ROUND: round_, Indices.CH: ch, Indices.Z: 0}
-    tile = z_projected_image.get_slice(indices)[0]
+    selector = {Axes.ROUND: round_, Axes.CH: ch, Axes.ZPLANE: 0}
+    tile = z_projected_image.get_slice(selector)[0]
     transformed = warp(tile, transform)
     z_projected_image.set_slice(
-        indices=indices,
+        selector=selector,
         data=transformed.astype(np.float32),
     )
 
@@ -54,6 +56,77 @@ for ch, round_, in slice_indices:
 ghp = starfish.image.Filter.GaussianHighPass(sigma=1)
 high_passed = ghp.run(z_projected_image, in_place=False)
 
+# ==================================== FINE REGISTRATION ==========================================
+
+# Rigid similarity registration is fit in the fourier domain. BaristaSeq requires somewhat more
+# complex registration that can fit shearing or non-rigid warpings. We cannot account for this at
+# the current time, but we can get close.
+
+# It may be possible to fit a piecewise rigid affine transformation, however we'll wait for the
+# ImageStack Cropping Framework for that.
+
+# TODO ambrosejcarr: Our cropping framework should be applicable to ImageStacks in addition to
+# experiments (it should run on xarrays!). This would get us closer to supportting the piecewise
+# rigid affine transform.
+
+# apply a transformation to some subset of the array and either (1) return result in-place or
+# return a complete array where part has been transformed.
+# TODO it would be nice to have a concatenate function so these transformations could be applied
+#      separately and then merged.
+
+
+channel_projected = z_projected_image.max_proj(Axes.CH)
+registered = deepcopy(z_projected_image)
+target_rounds = np.setdiff1d(z_projected_image.coordinates[Axes.ROUND.value].values, np.array([0]))
+for round_ in target_rounds:
+
+    template_selector = {
+        Axes.CH: 0,
+        Axes.ROUND: 0,
+        Axes.ZPLANE: 0,
+    }
+
+    subject_selector = {
+        Axes.CH: 0,
+        Axes.ROUND: round_,
+        Axes.ZPLANE: 0,
+    }
+
+    template = channel_projected.get_slice(template_selector)[0]
+    subject = channel_projected.get_slice(subject_selector)[0]
+    res = imr.similarity(template, subject)
+
+    similarity_matrix = SimilarityTransform(
+        scale=res['scale'],
+        rotation=res['angle'] * (np.pi / 180),  # convert to radians
+        translation=res['tvec'],
+    )
+
+    # apply the transform to each (r, c) pair
+    for ch in z_projected_image.coordinates[Axes.CH.value].values:
+        application_selector = {
+            Axes.CH: ch,
+            Axes.ROUND: round_,
+            Axes.ZPLANE: 0,
+        }
+        template = registered.get_slice(application_selector)[0]
+        transformed = warp(template, similarity_matrix)
+        registered.set_slice(
+            application_selector,
+            transformed.astype(np.float32),
+        )
+
+# ========================= ALTERNATE REGISTRATION (TRANSLATION ONLY) =============================
+
+fsr = starfish.image.Registration.FourierShiftRegistration(
+    upsampling=1000,
+    reference_stack=scaled.max_proj(Axes.CH),
+    verbose=True
+)
+registered = fsr.run(scaled)
+
+# TODO this is producing some weird artifacts, fix them!
+# there's a user warning about the size of the array changing. Could that be it?
 # ======================= CORRECT FOR BLEED THROUGH FOR ILLUMINA SBS ==============================
 
 # Illumina SBS reagents have significant spectral overlap. This linear unmixing step corrects
@@ -83,13 +156,13 @@ def do_bleed_correction(stack):
     bleed_corrected = deepcopy(stack)
 
     for index, (ch1, ch2, constant) in tqdm(bleed_correction_factors.iterrows()):
-        bleed = stack.get_slice({Indices.CH: int(ch1)})[0] * constant
-        img_to_correct = stack.get_slice({Indices.CH: int(ch2)})[0]
+        bleed = stack.get_slice({Axes.CH: int(ch1)})[0] * constant
+        img_to_correct = stack.get_slice({Axes.CH: int(ch2)})[0]
         corrected = np.maximum(img_to_correct - bleed, 0)
         bleed_corrected.set_slice(
-            {Indices.CH: int(ch2)},
+            {Axes.CH: int(ch2)},
             corrected,
-            axes=[Indices.ROUND, Indices.Z]
+            axes=[Axes.ROUND, Axes.ZPLANE]
         )
     return bleed_corrected
 
@@ -128,71 +201,10 @@ def scale_by_channel_percentile(data: np.ndarray, percentile: float) -> np.ndarr
 
 
 scaled = background_subtracted.apply(
-    scale_by_channel_percentile, group_by={Indices.CH}, in_place=False, verbose=True,
+    scale_by_channel_percentile, group_by={Axes.CH}, in_place=False, verbose=True,
     percentile=99.9
 )
 
-# ==================================== FINE REGISTRATION ==========================================
-
-# Rigid similarity registration is fit in the fourier domain. BaristaSeq requires somewhat more
-# complex registration that can fit shearing or non-rigid warpings. We cannot account for this at
-# the current time, but we can get close.
-
-# It may be possible to fit a piecewise rigid affine transformation, however we'll wait for the
-# ImageStack Cropping Framework for that.
-
-# TODO ambrosejcarr: Our cropping framework should be applicable to ImageStacks in addition to
-# experiments (it should run on xarrays!). This would get us closer to supportting the piecewise
-# rigid affine transform.
-
-# apply a transformation to some subset of the array and either (1) return result in-place or
-# return a complete array where part has been transformed.
-# TODO it would be nice to have a concatenate function so these transformations could be applied
-#      separately and then merged.
-
-
-channel_projected = scaled.max_proj(Indices.CH)
-registered = deepcopy(scaled)
-target_rounds = np.setdiff1d(scaled.coordinates[Indices.ROUND.value].values, np.array([0]))
-for round_ in target_rounds:
-
-    template_selector = {
-        Indices.CH.value: 0,
-        Indices.ROUND.value: 0,
-        Indices.Z.value: 0,
-    }
-
-    subject_selector = {
-        Indices.CH.value: 0,
-        Indices.ROUND.value: round_,
-        Indices.Z.value: 0,
-    }
-
-    template = channel_projected.get_slice(template_selector)[0]
-    subject = channel_projected.get_slice(subject_selector)[0]
-    res = imr.similarity(template, subject)
-
-    similarity_matrix = SimilarityTransform(
-        scale=res['scale'],
-        rotation=res['angle'] * (np.pi / 180),  # convert to radians
-        translation=res['tvec'],
-    )
-
-    # apply the transform to each (r, c) pair
-    for ch in scaled.coordinates[Indices.CH.value].values:
-        application_selector = {
-            Indices.CH.value: ch,
-            Indices.ROUND.value: round_,
-            Indices.Z.value: 0,
-        }
-        template = registered.get_slice(application_selector)[0]
-        transformed = warp(template, similarity_matrix)
-        registered.set_slice(
-            application_selector,
-            transformed.astype(np.float32),
-        )
-
-# TODO this is producing some weird artifacts, fix them!
 
 # ========================================= BLUR SPOTS ============================================
 
